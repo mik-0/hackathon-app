bcb27e29f3f07ec73da95b7ea4e8fa955d8fd98f
Mike Wu <23509561+mik-0@users.noreply.github.com>
2025-10-05 04:08:18 +0200

    force dark

--- apps/web/src/components/providers.tsx
+++ apps/web/src/components/providers.tsx
@@ -10,8 +10,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
 	return (
 		<ThemeProvider
 			attribute="class"
-			defaultTheme="system"
-			enableSystem
+			defaultTheme="dark"
+			forcedTheme="dark"
 			disableTransitionOnChange
 		>
 			<QueryClientProvider client={queryClient}>
